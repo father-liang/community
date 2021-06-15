e244cb31ad5beaf735ec42b2649abf1d8bcef2aa
Himma <2099180098@qq.com>
2021-06-15 22:03:10 +0800

    cookie first

--- src/main/java/com/controller/AuthorizeController.java
+++ src/main/java/com/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 
@@ -36,7 +38,8 @@ public class AuthorizeController {
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest httpServletRequest) {
+                           HttpServletRequest httpServletRequest,
+                           HttpServletResponse httpServletResponse) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
@@ -50,13 +53,15 @@ public class AuthorizeController {
 
         if (githubUser != null) {
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
             //登录成功，将请求得到的对象放入到session中
+            httpServletResponse.addCookie(new Cookie("token", token));
             httpServletRequest.getSession().setAttribute("user", user);
             //重定向回index页面
             return "redirect:/";
--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -1,15 +1,32 @@
 package com.controller;
 
+import com.mapper.UserMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    @Resource
+    private UserMapper userMapper;
+
     @GetMapping(value = "/")
-    public String Index(){
+    public String Index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+
+        for(Cookie cookie:cookies){
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+            }
+        }
+
         return "index";
     }
 }
