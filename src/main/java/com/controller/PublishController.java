bf25e6f44c87585841369a0c9e00e84dfc123bff
Himma <2099180098@qq.com>
2021-06-18 21:43:31 +0800

    lombok first

--- src/main/java/com/controller/PublishController.java
+++ src/main/java/com/controller/PublishController.java
@@ -4,6 +4,7 @@ import com.mapper.QuestionMapper;
 import com.model.Question;
 import com.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,22 +19,53 @@ public class PublishController {
     private QuestionMapper questionMapper;
 
     @GetMapping(value = "/publish")
-    public String publish(){
+    public String publish() {
         return "publish";
     }
 
     @PostMapping(value = "/publish")
     public String doPublish(Question question,
-                            HttpServletRequest request){
+                            HttpServletRequest request,
+                            Model model) {
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+
+        if (question.getTitle() == null || question.getTitle() == "") {
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+
+        if (question.getDescription() == null || question.getDescription() == "") {
+            model.addAttribute("error", "问题补充不能为空");
+            return "publish";
+        }
+
+        if (question.getTag() == null || question.getTag() == "") {
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+
         /*
-        * 我这里使用的是Session中的user提取用户的Id
-        * 视频里面采取的是获取Cookie，然后查询数据库
-        * */
-        User user = (User) request.getSession().getAttribute("user");
+         * 我这里使用的是Session中的user提取用户的Id
+         * 视频里面采取的是获取Cookie，然后查询数据库
+         * */
+
+        User user = null;
+        user = (User) request.getSession().getAttribute("user");
+
+        if (user == null) {
+            //出现异常，未登录，返回发布页面
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
 
         question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
         questionMapper.create(question);
-        return "publish";
+        return "redirect:/";
+
     }
 
 }
--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -1,5 +1,8 @@
 package com.model;
 
+import lombok.Data;
+
+@Data
 public class User {
     private int id;
     private String accountId;
@@ -7,64 +10,5 @@ public class User {
     private String token;
     private long gmtCreate;
     private long gmtModified;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public long getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(long gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public long getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(long gmtModified) {
-        this.gmtModified = gmtModified;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", accountId='" + accountId + '\'' +
-                ", name='" + name + '\'' +
-                ", token='" + token + '\'' +
-                ", gmtCreate=" + gmtCreate +
-                ", gmtModified=" + gmtModified +
-                '}';
-    }
+    private String avatarUrl;
 }
