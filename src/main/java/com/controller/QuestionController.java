761c4753a2378e8a6c7ce81d4bce77378924d11f
Himma <2099180098@qq.com>
2021-07-28 23:48:01 +0800

    comment commit question

--- src/main/java/com/controller/QuestionController.java
+++ src/main/java/com/controller/QuestionController.java
@@ -1,6 +1,9 @@
 package com.controller;
 
+import com.dto.CommentCreateDTO;
+import com.dto.CommentDTO;
 import com.dto.QuestionDTO;
+import com.service.CommentService;
 import com.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 public class QuestionController {
@@ -15,13 +19,19 @@ public class QuestionController {
     @Resource
     private QuestionService questionService;
 
+    @Resource
+    private CommentService commentService;
+
     @GetMapping("/question/{id}")
     public String question(@PathVariable(name = "id") Long id,
-                           Model model){
+                           Model model) {
         QuestionDTO questionDTO = questionService.getById(id);
+
+        List<CommentDTO> comments = commentService.listByQuestionId(id);
         //累加阅读数的功能
         questionService.incView(id);
         model.addAttribute("question", questionDTO);
+        model.addAttribute("comments", comments);
         return "question";
     }
 
--- src/main/java/com/service/CommentService.java
+++ src/main/java/com/service/CommentService.java
@@ -1,17 +1,25 @@
 package com.service;
 
+import com.dto.CommentDTO;
 import com.enums.CommentTypeEnum;
 import com.exception.CustomizeErrorCode;
 import com.exception.CustomizeException;
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.mapper.CommentMapper;
 import com.mapper.QuestionExtMapper;
 import com.mapper.QuestionMapper;
-import com.model.Comment;
-import com.model.Question;
+import com.mapper.UserMapper;
+import com.model.*;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -22,27 +30,30 @@ public class CommentService {
     @Resource
     private QuestionExtMapper questionExtMapper;
 
+    @Resource
+    private UserMapper userMapper;
+
     @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
         }
 
-        if(comment.getType() == null || !CommentTypeEnum.isExist(comment.getType())){
+        if (comment.getType() == null || !CommentTypeEnum.isExist(comment.getType())) {
             throw new CustomizeException(CustomizeErrorCode.TYPE_PARAM_WRONG);
         }
 
-        if(comment.getType() == CommentTypeEnum.COMMENT.getType()){
+        if (comment.getType() == CommentTypeEnum.COMMENT.getType()) {
             //回复评论
             Comment dbComment = commentMapper.selectByPrimaryKey(comment.getParentId());
-            if (dbComment == null){
+            if (dbComment == null) {
                 throw new CustomizeException(CustomizeErrorCode.COMMENT_NOT_FOUND);
             }
             commentMapper.insert(comment);
-        }else {
+        } else {
             //回复问题
             Question question = questionMapper.selectByPrimaryKey(comment.getParentId());
-            if(question == null){
+            if (question == null) {
                 throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
             }
             commentMapper.insert(comment);
@@ -50,5 +61,37 @@ public class CommentService {
             questionExtMapper.incCommentCount(question);
 
         }
+    }
+
+    public List<CommentDTO> listByQuestionId(Long id) {
+        CommentExample commentExample = new CommentExample();
+        commentExample.createCriteria()
+                .andParentIdEqualTo(id)
+                .andTypeEqualTo(CommentTypeEnum.QUESTION.getType());
+        commentExample.setOrderByClause("gmt_create desc");
+        List<Comment> comments = commentMapper.selectByExample(commentExample);
+
+        if (comments.size() == 0) {
+            return new ArrayList<>();
+        }
+        Set<Long> commentators = comments.stream().map(comment -> comment.getCommentator()).collect(Collectors.toSet());
+        ArrayList<Long> userIds = new ArrayList<>();
+        userIds.addAll(commentators);
+
+        UserExample userExample = new UserExample();
+        userExample.createCriteria()
+                .andIdIn(userIds);
+        List<User> users = userMapper.selectByExample(userExample);
+
+        Map<Long, User> userMap = users.stream().collect(Collectors.toMap(user -> user.getId(), user -> user));
+
+        List<CommentDTO> commentDTOS = comments.stream().map(comment -> {
+            CommentDTO commentDTO = new CommentDTO();
+            BeanUtils.copyProperties(comment, commentDTO);
+            commentDTO.setUser(userMap.get(comment.getCommentator()));
+            return commentDTO;
+        }).collect(Collectors.toList());
+
+        return commentDTOS;
     }
 }
