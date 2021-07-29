400a76561eefd3e4766f269f1c2a1782f5b898fd
Himma <2099180098@qq.com>
2021-07-29 23:50:13 +0800

    sub comment first

--- target/classes/static/js/community.js
+++ target/classes/static/js/community.js
@@ -1,10 +1,13 @@
+/*
+* 提交回复
+* */
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
 
-    if (!content){
+    if (!content) {
         alert("不能回复空内容");
-        return ;
+        return;
     }
 
     $.ajax({
@@ -22,7 +25,7 @@ function post() {
             } else {
                 if (response.code == 2003) {
                     var isAccepted = confirm(response.message);
-                    if (isAccepted){
+                    if (isAccepted) {
                         window.open("https://github.com/login/oauth/authorize?client_id=Iv1.9ea01843dcda5ae5&redirect_uri=http://localhost:8080/callback&scope=user&state=1")
                         window.localStorage.setItem("closable", true);
                     }
@@ -36,5 +39,31 @@ function post() {
         },
         dataType: "json"
     });
+
+}
+
+/*
+* 展开二级评论
+* */
+function collapseComments(e) {
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-" + id);
+
+    //获取二级评论的展开状态
+    var collapse = e.getAttribute("data-collapse");
+    if (collapse) {
+        //折叠二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    } else {
+        //展开二级评论
+        comments.addClass("in");
+
+        //标记二级评论展开状态
+        e.setAttribute("data-collapse", "in");
+        e.classList.add("active");
+    }
+
 
 }
